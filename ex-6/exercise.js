f139fd6192e6da76c06b4c85faa74d5799b31cd6
amie senghore <amiesenghore1@gmail.com>
2021-02-25 17:13:18 +0100

    exercise6js

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,4 @@
-// Example
-window.alert("Hello World!");
+
+var author = prompt("put the name of the author");
+var quote = prompt("put the quote of author");
+window.alert(author + " say " + quote);
