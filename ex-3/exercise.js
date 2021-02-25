ec3679da1117bdd72bfa5d945d2d211341c2cd99
amie senghore <amiesenghore1@gmail.com>
2021-02-25 14:59:16 +0100

    exercise3js

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,12 @@
-// Example
-window.alert("Hello World!");
+
+var variable1=10;
+var variable2=5;
+if (variable1>variable2) {
+    window.alert ( "The greater number of " + variable1 + " and " + variable2 + " is " + variable1 + ".");
+}
+
+else if(variable1<variable2) {
+    window.alert ( "The greater number of " + variable1 + " and " + variable2 + " is " + variable2 + ".");
+} else {
+    window.alert ("The numbers are the same.")
+}
