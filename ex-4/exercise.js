8fa951dc5d5c90c2be447f6796bcbde1464a71a1
amie senghore <amiesenghore1@gmail.com>
2021-02-25 15:53:05 +0100

    exercise4js

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,15 @@
-// Example
-window.alert("Hello World!");
+
+var myName = prompt("put your name");
+window.alert("Hello "+ myName);
+
+var yourDay = prompt("How your day was good or bad");
+if(yourDay === "good" || yourDay === "GOOD")
+
+{
+    window.alert("Im happy for you champ");
+}
+
+ else {
+   window.alert("thats too bad,tomorrow its a new day");
+}
+
