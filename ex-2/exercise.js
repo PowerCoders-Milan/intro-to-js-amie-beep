54d348d8d6cc99f1d8d5f43f01d03595abd87678
amie senghore <amiesenghore1@gmail.com>
2021-02-25 13:13:00 +0100

    exercise2js

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,5 @@
-// Example
-window.alert("Hello World!");
+
+var birthYear=1996;
+var futureYear=2022;
+var age=futureYear-birthYear;
+window.alert ("I will be either " + age + " or " + (age+1) + " in " + futureYear);
