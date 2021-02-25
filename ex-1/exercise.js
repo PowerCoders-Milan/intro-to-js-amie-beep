2e47e8bcdb4e38feffccab65421ee37bacff5071
amie senghore <amiesenghore1@gmail.com>
2021-02-25 12:06:42 +0100

    exercise1js

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,9 @@
 // Example
 window.alert("Hello World!");
+var numberOfChildren=2;
+var partnersName="Yusuf";
+var geographicLocation="Torino";
+var jobTitle="Accountant";
+
+window.alert("You will be an " +  jobTitle + " in " + geographicLocation + " , and married to " + partnersName + " with " + numberOfChildren + " kids.");
+console.log ("You will be an " +  jobTitle + " in " + geographicLocation + " , and married to " + partnersName + " with " + numberOfChildren + " kids.");
